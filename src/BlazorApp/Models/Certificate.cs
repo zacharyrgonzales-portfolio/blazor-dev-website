6f7b405f53118d665fbff8264c38a5db80af750e
zacharyrgonzales-azure <zacharyrgonzales.azure@gmail.com>
2023-06-08 00:46:18 -0500

    update education page

---
+++ src/BlazorApp/Models/Certificate.cs
@@ -0,0 +1,8 @@
+namespace BlazorApp.Models;
+
+public class Certificate
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+}
